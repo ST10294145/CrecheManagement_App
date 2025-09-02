0e3b8c0cdf135b412159e386324945745905d4e4
Nehara Pillay <ST10198206@vcconnect.edu.za>
2025-09-02 09:42:55 +0200

    Updated activity_register.xml

--- app/src/main/java/com/crecheconnect/crechemanagement_app/RegisterActivity.kt
+++ app/src/main/java/com/crecheconnect/crechemanagement_app/RegisterActivity.kt
@@ -1,20 +1,56 @@
 package com.crecheconnect.crechemanagement_app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.LinearLayout
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
+        // Make sure your root layout in activity_register.xml has android:id="@+id/main"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val roleSpinner: Spinner = findViewById(R.id.roleSpinner)
+        val parentSection: LinearLayout = findViewById(R.id.parentSection)
+        val staffSection: LinearLayout = findViewById(R.id.staffSection)
+        val adminSection: LinearLayout = findViewById(R.id.adminSection)
+
+        roleSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                parentSection.visibility = View.GONE
+                staffSection.visibility = View.GONE
+                adminSection.visibility = View.GONE
+
+                when (position) {
+                    0 -> parentSection.visibility = View.VISIBLE
+                    1 -> staffSection.visibility = View.VISIBLE
+                    2 -> adminSection.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
         }
     }
 }

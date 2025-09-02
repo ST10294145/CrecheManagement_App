fc461a7b689899e5cb3db925ea5ca39544842af5
Nehara Pillay <ST10198206@vcconnect.edu.za>
2025-09-02 09:25:56 +0200

    Updated Welcome Page and added logic to buttons

--- app/src/main/java/com/crecheconnect/crechemanagement_app/MainActivity.kt
+++ app/src/main/java/com/crecheconnect/crechemanagement_app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.crecheconnect.crechemanagement_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            insets // ✅ return insets properly here
+        }
+
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnCreateAccount: Button = findViewById(R.id.btnCreateAccount)
+        btnCreateAccount.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }

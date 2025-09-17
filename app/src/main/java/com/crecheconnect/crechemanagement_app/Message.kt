b4b9693a0d8f555fc3925faa62d2e2ddbffb5ba5
st10294145 <st10294145@vcconnect.edu.za>
2025-09-17 11:27:56 +0200

    Updated Message.kt
    Default values give Firestore something to map to.
    
    receiverId = "all" can be used to broadcast to all parents (we’ll query for ["all", currentParentId]).

--- app/src/main/java/com/crecheconnect/crechemanagement_app/Message.kt
+++ app/src/main/java/com/crecheconnect/crechemanagement_app/Message.kt
@@ -1,6 +1,10 @@
 package com.crecheconnect.crechemanagement_app
 
 data class Message(
-    val title: String,
-    val body: String
+    val id: String = "",
+    val title: String = "",
+    val body: String = "",
+    val senderId: String = "",
+    val receiverId: String = "", // "all" for broadcast, or a specific parent UID
+    val timestamp: Long = System.currentTimeMillis()
 )

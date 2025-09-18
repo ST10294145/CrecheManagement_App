aaf30bcfdb763bd657aff712bef67958c57de7f9
st10294145 <st10294145@vcconnect.edu.za>
2025-09-18 10:26:13 +0200

    Created ProfileActivity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.androidx.activity)
 
     // Testing
     testImplementation("junit:junit:4.13.2")

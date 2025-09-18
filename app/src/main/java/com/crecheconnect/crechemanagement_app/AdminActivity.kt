f82c569daadc841a0b64bd513b421ad5c8390380
st10294145 <st10294145@vcconnect.edu.za>
2025-09-18 10:13:46 +0200

    Updated AdminActivity.kt

--- app/src/main/java/com/crecheconnect/crechemanagement_app/AdminActivity.kt
+++ app/src/main/java/com/crecheconnect/crechemanagement_app/AdminActivity.kt
@@ -1,6 +1,8 @@
 package com.crecheconnect.crechemanagement_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class AdminActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnRegistration: Button = findViewById(R.id.btnRegistration)
+        btnRegistration.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }

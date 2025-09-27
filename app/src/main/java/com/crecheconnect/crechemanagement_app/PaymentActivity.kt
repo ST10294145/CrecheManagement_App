a9168fb1e8ac4811db350255bb6168e18ee25acc
st10294145 <st10294145@vcconnect.edu.za>
2025-09-27 16:55:52 +0200

    Updated PaymentActivity.kt
    FIxed all errors

--- app/src/main/java/com/crecheconnect/crechemanagement_app/PaymentActivity.kt
+++ app/src/main/java/com/crecheconnect/crechemanagement_app/PaymentActivity.kt
@@ -1,20 +1,36 @@
-package com.crecheconnect.crechemanagement_app
+package com.crecheconnect.crechemanagement_app.payment
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.crecheconnect.crechemanagement_app.R
+import com.crecheconnect.crechemanagement_app.PayfastWebViewActivity
 
 class PaymentActivity : AppCompatActivity() {
+
+    private lateinit var amountEditText: EditText
+    private lateinit var itemNameEditText: EditText
+    private lateinit var payButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_payment)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_payment) // your parent activity XML
+
+        amountEditText = findViewById(R.id.amountEditText)
+        itemNameEditText = findViewById(R.id.itemNameEditText)
+        payButton = findViewById(R.id.payButton)
+
+        payButton.setOnClickListener {
+            val amount = amountEditText.text.toString().ifEmpty { "100.00" }
+            val itemName = itemNameEditText.text.toString().ifEmpty { "Creche Payment" }
+
+            val intent = Intent(this, PayfastWebViewActivity::class.java).apply {
+                putExtra(PayfastWebViewActivity.EXTRA_AMOUNT, amount)
+                putExtra(PayfastWebViewActivity.EXTRA_ITEM_NAME, itemName)
+            }
+            startActivity(intent)
         }
     }
 }
